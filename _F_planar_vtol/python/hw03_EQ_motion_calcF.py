1d04c24534d9ac1a2a1ba3d51ae8cfb9495de5b7
cpaul-mech <cpaul.mech@gmail.com>
2024-02-09 23:24:44 -0700

    finished Assignnent 4

--- _F_planar_vtol/python/hw03_EQ_motion_calcF.py
+++ _F_planar_vtol/python/hw03_EQ_motion_calcF.py
@@ -16,32 +16,32 @@ from sympy import sin, cos, diff, Matrix, symbols, Function, pretty_print, simpl
 #defining mathematical variables (called symbols in sp) and time varying functions like z and theta
 t, m_l, m_r, m_c, d, g, J_c = symbols('t, m_l, m_r, m_c, d, g, J_c')
 
-x_v = dynamicsymbols('x_v')
+z = dynamicsymbols('z')
 h = dynamicsymbols('h')
 theta = dynamicsymbols('theta')
 
 
 #defining generalized coords and derivatives
-q = Matrix([[h], [x_v], [theta]])
+q = Matrix([[z], [h], [theta]])
 qdot = q.diff(t)
 
 #defining the kinetic energy
-p_mc = Matrix([[x_v], [h], [0]])
-p_mr = Matrix([[x_v + d*cos(theta)], [h+sin(theta)*d], [0]])
-p_ml = Matrix([[x_v - d*cos(theta)], [h-sin(theta)*d], [0]])
+p_mc = Matrix([[z], [h], [0.]])
+p_mr = Matrix([[z + d*cos(theta)], [h + d*sin(theta)], [0.]])
+p_ml = Matrix([[z - d*cos(theta)], [h - d*sin(theta)], [0.]])
 
-v_mc = diff(p_mc, t)
-v_mr = diff(p_mr, t)
-v_ml = diff(p_ml, t)
+v_c = diff(p_mc, t)
+v_r = diff(p_mr, t)
+v_l = diff(p_ml, t)
 
 
 omega = Matrix([[0], [0], [diff(theta,t)]])
 J = Matrix([[0, 0, 0], [0, 0, 0], [0, 0, J_c]])
 
 #%%
-m_l = m_r
-K = simplify(0.5*m_l*v_ml.T*v_ml + 0.5*m_r*v_mr.T*v_mr + 0.5*omega.T*J*omega + 0.5*m_c*v_mc.T*v_mc)
-
+K = (0.5*m_c*v_c.T*v_c + 0.5*omega.T*J*omega + 0.5*m_r*v_r.T*v_r + 0.5*m_l*v_l.T*v_l)
+K = K.subs(m_l, m_r)
+K = simplify(K)
 display(Math(vlatex(K)))
 # just grabbing the scalar inside this matrix so that we can do L = K-P, since P is a scalar
 K = K[0,0]
@@ -49,27 +49,38 @@ K = K[0,0]
 #%%
 #defining potential energy 
 P = m_c*g*h + m_r*g*(h+sin(theta)*d) + m_l*g*(h-sin(theta)*d)
-
+P = P.subs(m_l, m_r)
+P = simplify(P)
+display(Math(vlatex(P)))
+#%%
 #calculate the lagrangian, using simplify intermittently can help the equations to be
 #simpler, there are also options for factoring and grouping if you look at the sympy
 #documentation.
+
 L = simplify(K-P)
+
+
 print("L =")
 display(Math(vlatex(L)))
 
 #%%
 # Solution for Euler-Lagrange equations, but this does not include right-hand side (like friction and tau)
 Left_side_EQ_Motion_E = simplify( diff(diff(L, qdot), t) - diff(L, q) )
-
+print("Left Hand Side EOM =")
 display(Math(vlatex(Left_side_EQ_Motion_E)))
 
 #%%
 ## Define Generalized Forces
 # defining symbols for external force and friction
-F, tau, ell = symbols('F tau ell')
-r1 = Matrix([[ell*cos(theta)], [ell*sin(theta)], [0]])
-F1 = Matrix([[0], [F], [0]])
-
+F, tau = symbols('F tau')
+RHS = Matrix([[-F*sin(theta)],[F*cos(theta)], [tau]])
+print("Right Hand Side EOM =")
+display(Math(vlatex(RHS)))
+#%%
+# defining the full equation of motion
+full_eom = Left_side_EQ_Motion_E - RHS
+print("Full EOM =")
+display(Math(vlatex(full_eom)))
 #%% 
 ############################################################
 ### Including friction and generalized forces, then solving for highest order derivatives
@@ -80,66 +91,24 @@ zd = z.diff(t)
 zdd = zd.diff(t)
 thetad = theta.diff(t)
 thetadd = thetad.diff(t)
+hd = h.diff(t)
+hdd = hd.diff(t)
 
-
-# defining the right-hand side of the equation and combining it with E-L part
-RHS = Q
-full_eom = Left_side_EQ_Motion_E - RHS
 
 #%%
 # finding and assigning zdd and thetadd
-result = simplify(sp.solve(full_eom, (zdd, thetadd)))
+result = simplify(sp.solve(full_eom, (zdd, hdd, thetadd)))
 
 # result is a Python dictionary, we get to the entries we are interested in
 # by using the name of the variable that we were solving for
 zdd_eom = result[zdd]  # EOM for zdd, as a function of states and inputs
+hdd_eom = result[hdd]  # EOM for hdd, as a function of states and inputs
 thetadd_eom = result[thetadd] # EOM for thetadd, as a function of states and inputs
-
+print("zdd_eom =")
 display(Math(vlatex(zdd_eom)))
+print("hdd_eom =")
+display(Math(vlatex(hdd_eom)))
+print("thetadd_eom =")
 display(Math(vlatex(thetadd_eom)))
-
-mass_matrix = Matrix([[m_l+m_r, m_l*ell/2*cos(theta)], [m_l*ell/2*cos(theta), m_l*ell**2/3]])
-
-#%% [markdown]
-# Let's take a look at the inverse of the mass matrix (to compare against the solution shown in the book). 
-
-# %%
-display(Math(vlatex(mass_matrix.inv())))
-
-
-
-#%% [markdown]
-# OK, now we can see if I can get the state variable form of the equations of motion.
-
-#%%
-# defining fixed parameters that are not states or inputs (like g, ell, m_l, m_r, b)
-params = [(m_l, 0.25), (m_r, 1.), (ell, 1.), (g, 9.8), (b, 0.05)] #this a python list of tuples.
-
-# substituting parameters into the equations of motion
-zdd_eom = zdd_eom.subs(params)
-thetadd_eom = thetadd_eom.subs(params)
-
-# now defining the state variables that will be passed into f(x,u) 
-state = [zd, z, thetad, theta]
-ctrl_input = [F]
-
-# defining the function that will be called to get the derivatives of the states
-state_dot = [zdd_eom, zd, thetadd_eom, thetad]
-
-# converting the function to a callable function that uses numpy to evaluate and 
-# return a list of state derivatives
-f = sp.lambdify(state + ctrl_input, state_dot)
-
-# calling the function as a test to see if it works, could do something like this 
-# in a simulation
-
-# f(zd, z, thetad, theta, F)
-print("x_dot = ", f(0, 0, 0, 0, 1))
-
-#%% [markdown] 
-# Although not covered in this class, there are ways to store this function and just re-load it
-# when you want to use it, so that you don't have to re-run the sympy code every time you want to
-# perform a simulation. See https://stackoverflow.com/questions/29079923/save-load-sympy-lambdifed-expressions
-
 
 # %%
--- _F_planar_vtol/python/hw_04F_VTOL_EQ.py
+++ _F_planar_vtol/python/hw_04F_VTOL_EQ.py
@@ -1,5 +1,5 @@
 #%%
-from hw03_EQ_motion_calcE import *
+from hw03_EQ_motion_calcF import *
 
 
 #%%
@@ -11,36 +11,44 @@ from hw03_EQ_motion_calcE import *
 # defining derivative of states, states, and inputs symbolically
 ### for this first one, keep in mind that zdd_eom is actually a full
 ### row of equations, while zdd is just the symbolic variable itself. 
-state_variable_form = Matrix([[zdd_eom], [zd], [thetadd_eom], [thetad]])
-states = Matrix([[zd], [z], [thetad], [theta]])
-inputs = Matrix([[F]])
+state_variable_form = Matrix([[zdd_eom], [zd], [hdd_eom], [hd], [thetadd_eom], [thetad]])
+states = Matrix([[zd], [z], [hd], [h], [thetad], [theta]])
+inputs = Matrix([[F], [tau]])
 print("state_variable_form =")
 display(Math(vlatex(state_variable_form)))
 print("states =")
 display(Math(vlatex(states)))
 print("inputs =")
 display(Math(vlatex(inputs)))
-# %% [markdown]
+
+#%% [markdown]
 # Set all the derivatives to zero and solve for the equilibrium points.
-F_e, z_e, theta_e = symbols('F_e, z_e, theta_e')
-equilibrium_system = state_variable_form.subs([(zdd,0.), (zd,0.),(F, F_e), (z, z_e), (thetad, 0.), (thetadd, 0.), (theta, theta_e)])
-equilibrium_system = simplify(equilibrium_system)
+z_e, h_e, theta_e, F_e, tau_e = symbols('z_e h_e theta_e F_e tau_e')
+equilibrium_system = state_variable_form.subs([(zdd,0.), (zd,0.), (z, z_e), (hdd, 0.), (hd, 0.), (h, h_e), (thetadd, 0.), (thetad, 0.), (theta, theta_e), (F, F_e), (tau, tau_e)])
 print("Equilibrium system: ")
 display(Math(vlatex(equilibrium_system)))
-equilibrium_points = sp.solve(equilibrium_system, (F_e))
-print("Equilibrium solved for F_e: ", equilibrium_points)
+equilibrium_system = equilibrium_system.subs(theta_e, 0.)
+print("Equilibrium system with theta_e = 0: ")
+display(Math(vlatex(equilibrium_system)))
+equilibrium_points = sp.solve(equilibrium_system, (F_e, tau_e))
+print("Equilibrium solved for F_e & tau_e: ", equilibrium_points)
 display(Math(vlatex(equilibrium_points)))
+#%%
+F_e = equilibrium_points[F_e]
+tau_e = equilibrium_points[tau_e]
+
 #%%
 # finding the jacobian with respect to states (A) and inputs (B)
 A = state_variable_form.jacobian(states)
 B = state_variable_form.jacobian(inputs)
 
 # sub in values for equilibrium points 
-A_lin = simplify(A.subs([(thetad,0.), (theta,0.), (zd,0.), (z,0.), (F, 0.)]))
-B_lin = simplify(B.subs([(thetad,0.), (theta,0.), (zd,0.), (z,0.), (F, 0.)]))
+A_lin = simplify(A.subs([(zd,0.), (z,0.), (hd,0.), (h,0.), (thetad,0.), (theta,0.), (F, F_e), (tau, 0.)]))
+B_lin = simplify(B.subs([(zd,0.), (z,0.), (hd,0.), (h,0.), (thetad,0.), (theta,0.), (F, F_e), (tau, 0.)]))
 print("A_lin =")
 display(Math(vlatex(A_lin)))
 print("B_lin =")
 display(Math(vlatex(B_lin)))
+
 
 # %%
