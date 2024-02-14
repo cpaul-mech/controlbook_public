5dca06584a559cbbb6cf016ef419740650f7cecf
cpaul-mech <cpaul.mech@gmail.com>
2024-02-14 14:48:27 -0700

    finished first HW problem HW5

--- _D_mass/python/hw05_DmassSpring_laplace.py
+++ _D_mass/python/hw05_DmassSpring_laplace.py
@@ -1,7 +1,15 @@
 #%% [markdown]
-# Laplace Transform of the Mass-Spring-Damper System
+## Laplace Transform and Transfer Function of the Mass-Spring-Damper System
 # Importing the necessary libraries and defining the symbols
 from hw04_DmassSpring_EQ import *
+#%% [markdown]
+# Import the Laplace transform function
+from sympy import laplace_transform
+t, s = symbols('t s')
+# Take the laplace transform of the state_variable_form
+state_variable_form_laplace = laplace_transform(state_variable_form, t, s, noconds=True)
+print("State variable form in Laplace domain:")
+display(Math(vlatex(state_variable_form_laplace)))
 
 #%% [markdown]
 # Define A, B, C, and D for the system
