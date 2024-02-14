974cd145c2f0eaba3fc5c2afaed15016310f9d5d
cpaul-mech <cpaul.mech@gmail.com>
2024-02-14 16:30:32 -0700

    lots of changes

--- _hummingbird_sim/lab3_hummingbirdSim.py
+++ _hummingbird_sim/lab3_hummingbirdSim.py
@@ -5,7 +5,7 @@ from signalGenerator import SignalGenerator
 from hummingbirdAnimation import HummingbirdAnimation
 from dataPlotter import DataPlotter
 from hummingbirdDynamics import HummingbirdDynamics
-
+from ctrlEquilibrium import ctrlEquilibrium
 # instantiate reference input classes
 phi_ref = SignalGenerator(amplitude=1.5, frequency=0.05)
 theta_ref = SignalGenerator(amplitude=0.5, frequency=0.05)
@@ -17,20 +17,22 @@ force_r_ref = SignalGenerator(amplitude=1.0, frequency=.03)
 dataPlot = DataPlotter()
 animation = HummingbirdAnimation()
 dynamics = HummingbirdDynamics()
+ctrlr = ctrlEquilibrium()
 
 t = P.t_start  # time starts at t_start
 while t < P.t_end:  # main simulation loop
     t_next_plot = t + P.t_plot  # set the next time to plot
     # set variables
     while t< t_next_plot:
-        force = np.array([[force_ell_ref.sin(t)], [force_r_ref.sin(t)]])
         state = dynamics.state
+        force_pwm = ctrlr.update(state)
+        
         ref = np.array([[0.0], [0.0], [0.0]])
-        dynamics.update(force)
+        dynamics.update(force_pwm)
         t = t + P.Ts
         
     animation.update(t, state)
-    dataPlot.update(t, state, ref, force)
+    dataPlot.update(t, state, ref, force_pwm)
     t = t + P.t_plot  # advance time by t_plot
     plt.pause(0.05)
 
